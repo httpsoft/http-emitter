9ec1bc0d577bbca9c1d3b543efee8701aa384ebd
devanych <mail@devanych.ru>
2020-06-30 21:45:36 +0300

    Add EmitterInterface

---
+++ src/EmitterInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Runner;
+
+use Psr\Http\Message\ResponseInterface;
+
+interface EmitterInterface
+{
+    /**
+     * Emits a HTTP response, that including status line, headers and message body, according to the environment.
+     *
+     * When implementing this method, MAY use `header()` and the output buffer. Also implementations MAY throw
+     * exceptions if cannot emit a response, e.g., if headers already sent or output has been emitted previously.
+     *
+     * @param ResponseInterface $response
+     */
+    public function emit(ResponseInterface $response): void;
+}
